cff32c12ad492de41ca61f9016e72f80a143226e
Marek Jeliński <markandjel@gmail.com>
2024-11-30 15:15:13 +0100

    Improve error handling in medrxiv paper fetcher and remove date constraints

--- data/medrxiv_paper_fetcher.py
+++ data/medrxiv_paper_fetcher.py
@@ -11,9 +11,7 @@ logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-if __name__ == '__main__':
-    begin_date = "2024-11-01"
-    end_date = "2024-11-29"
+if __name__ == "__main__":    
     api = MedRxivApi(max_retries=10)
     csv_path = pathlib.Path("medrxiv_abstracts.csv")
     csv_path.parent.mkdir(parents=True, exist_ok=True)
@@ -24,15 +22,19 @@ if __name__ == '__main__':
         doi_numbers = set()
         paper_counter = 0
 
-        for paper in api.get_papers(begin_date=begin_date, end_date=end_date):
+        for paper in api.get_papers():
             if paper["doi"] in doi_numbers:
                 continue
 
             doi_numbers.add(paper["doi"])
             parsed_abstract = remove_latex_tags(paper["abstract"])
-            writer.writerow([parsed_abstract, paper["doi"]])
-            paper_counter += 1
-            if paper_counter % 100 == 0:
+            try:
+                writer.writerow([parsed_abstract, paper["doi"]])
+                paper_counter += 1
+            except UnicodeEncodeError:
+                logging.error(f"Failed to write paper with DOI: {paper['doi']}")
+                continue
+            if paper_counter % 1000 == 0:
                 logging.info(f"Downloaded {paper_counter} articles.")
 
     logging.info(f"Fetching done downloading {paper_counter} articles.")
