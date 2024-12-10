37361f2488d8c68b7dc989c4e857b05e33f00d15
markjeli <markandjel@gmail.com>
2024-12-10 17:39:40 +0100

    Simplify code

--- data/medical_paper_fetcher.py
+++ data/medical_paper_fetcher.py
@@ -27,7 +27,29 @@ BIORXIV_CATEGORIES = [
     "physiology",
 ]
 
-if __name__ == "__main__":    
+
+def fetch_papers(api, fields, counter, categories=None):
+    for paper in api.get_papers(fields=fields):
+        if (categories and paper["category"].lower() not in categories) or paper[
+            "doi"
+        ] in doi_numbers:
+            continue
+        doi_numbers.add(paper["doi"])
+        parsed_abstract = remove_latex_tags(paper["abstract"])
+        try:
+            writer.writerow([parsed_abstract, paper["doi"]])
+            counter += 1
+        except UnicodeEncodeError:
+            logging.error(f"Failed to write paper with DOI: {paper['doi']}")
+            continue
+        if counter % 1000 == 0:
+            logging.info(
+                f"Downloaded {counter} articles from {api.__class__.__name__}."
+            )
+    return counter
+
+
+if __name__ == "__main__":
     medrxiv_api = MedRxivApi(max_retries=10)
     biorxiv_api = BioRxivApi(max_retries=10)
     csv_path = pathlib.Path("medical_abstracts.csv")
@@ -35,41 +57,23 @@ if __name__ == "__main__":
 
     with open(csv_path, "a", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["Abstract", "Link"])
+        writer.writerow(["Abstract", "DOI"])
         doi_numbers = set()
         medrxiv_paper_counter = 0
         biorxiv_paper_counter = 0
 
         # Fetch papers from medRxiv
-        for paper in medrxiv_api.get_papers(fields=["abstract", "doi"]):
-            if paper["doi"] in doi_numbers:
-                continue
-
-            doi_numbers.add(paper["doi"])
-            parsed_abstract = remove_latex_tags(paper["abstract"])
-            try:
-                writer.writerow([parsed_abstract, paper["doi"]])
-                medrxiv_paper_counter += 1
-            except UnicodeEncodeError:
-                logging.error(f"Failed to write paper with DOI: {paper['doi']}")
-                continue
-            if medrxiv_paper_counter % 1000 == 0:
-                logging.info(f"Downloaded {medrxiv_paper_counter} articles from MedRxiv.")
-
+        medrxiv_paper_counter = fetch_papers(
+            medrxiv_api, ["abstract", "doi"], medrxiv_paper_counter
+        )
         # Fetch papers from bioRxiv
-        for paper in biorxiv_api.get_papers(fields=["abstract", "doi", "category"]):
-            if paper['category'].lower() not in BIORXIV_CATEGORIES or paper["doi"] in doi_numbers:
-                continue
+        biorxiv_paper_counter = fetch_papers(
+            biorxiv_api,
+            ["abstract", "doi", "category"],
+            biorxiv_paper_counter,
+            BIORXIV_CATEGORIES,
+        )
 
-            doi_numbers.add(paper["doi"])
-            parsed_abstract = remove_latex_tags(paper["abstract"])
-            try:
-                writer.writerow([parsed_abstract, paper["doi"]])
-                biorxiv_paper_counter += 1
-            except UnicodeEncodeError:
-                logging.error(f"Failed to write paper with DOI: {paper['doi']}")
-                continue
-            if biorxiv_paper_counter % 1000 == 0:
-                logging.info(f"Downloaded {biorxiv_paper_counter} articles from BioRxiv.")
-
-    logging.info(f"Fetching done downloading {medrxiv_paper_counter + biorxiv_paper_counter} articles.")
+    logging.info(
+        f"Fetching done downloading {medrxiv_paper_counter + biorxiv_paper_counter} articles."
+    )
