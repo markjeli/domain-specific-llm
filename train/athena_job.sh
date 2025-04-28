b5e044b3a68327d92278ddd0f735702e8dfe4ca1
markjeli <markandjel@gmail.com>
2025-04-28 16:17:37 +0200

    Refactor training script to accept configuration parameters and update job script for training settings

--- train/athena_job.sh
+++ train/athena_job.sh
@@ -19,6 +19,13 @@ source $SCRATCH/venv/bin/activate
 export HF_HOME=$SCRATCH/.cache_dir/huggingface
 export PIP_CACHE_DIR=$SCRATCH/.cache_dir/pip
 
-python train_cpt.py
+python train_cpt.py \
+  --dataset_text_field Abstract \
+  --max_seq_length 2048 \
+  --per_device_train_batch_size 8 \
+  --gradient_accumulation_steps 8 \
+  --learning_rate 2e-5 \
+  --optim adamw_8bit \
+  --output_dir $SCRATCH/model-outputs
 
 # it is important to start this script with: sbatch athena_job.sh
--- train/train_cpt.py
+++ train/train_cpt.py
@@ -1,17 +1,16 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, HfArgumentParser
 from trl import SFTConfig, SFTTrainer
 
 
-def main():
+def main(sft_config):
     dataset_path = "medical_abstracts_train.csv"
     model_name_or_path = "meta-llama/Llama-3.2-1B"
-    output_dir = "outputs"
 
     tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
     model = AutoModelForCausalLM.from_pretrained(model_name_or_path, device_map="auto")
 
-    dataset = load_dataset("csv", data_files=dataset_path, split="train[:2500]")
+    dataset = load_dataset("csv", data_files=dataset_path, split="train")
 
     EOS_TOKEN = tokenizer.eos_token
     tokenizer.pad_token = "<|finetune_right_pad_id|>"
@@ -21,20 +20,10 @@ def main():
 
     dataset = dataset.map(formatting_prompts_func, batched=True)
 
-    training_args = SFTConfig(
-        dataset_text_field="Abstract",
-        max_seq_length=2048,
-        per_device_train_batch_size=2,
-        gradient_accumulation_steps=8,
-        learning_rate=2e-5,
-        optim="adamw_8bit",
-        output_dir=output_dir,
-    )
-
     trainer = SFTTrainer(
         model=model,
         train_dataset=dataset,
-        args=training_args,
+        args=sft_config,
         processing_class=tokenizer,
     )
 
@@ -45,4 +34,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = HfArgumentParser(SFTConfig)
+    sft_config = parser.parse_args_into_dataclasses()
+    main(sft_config)
